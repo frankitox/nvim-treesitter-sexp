010eddf0f7beaadc89e6309a872e081cc445f86e
Franco Biasin <franconbiasin+github@gmail.com>
2026-04-15 11:32:43 -0300

    Remove nvim-treesitter dependency
    nvim-treesitter's main branch removed update_selection

--- lua/treesitter-sexp/textobjects.lua
+++ lua/treesitter-sexp/textobjects.lua
@@ -1,5 +1,18 @@
-local ts_utils = require "nvim-treesitter.ts_utils"
 local utils = require "treesitter-sexp.utils"
+
+local function update_selection(bufnr, range, mode)
+  local start_row, start_col, end_row, end_col = unpack(range)
+  -- end_col from treesitter is exclusive, visual selection needs inclusive
+  if end_col > 0 then
+    end_col = end_col - 1
+  else
+    end_row = end_row - 1
+    end_col = #vim.api.nvim_buf_get_lines(bufnr, end_row, end_row + 1, false)[1] - 1
+  end
+  vim.api.nvim_buf_set_mark(bufnr, "<", start_row + 1, start_col, {})
+  vim.api.nvim_buf_set_mark(bufnr, ">", end_row + 1, end_col, {})
+  vim.cmd("normal! gv")
+end
 
 ---@type table<string, TSSexp.Textobject>
 local M = {
@@ -64,7 +77,7 @@ local metatable = {
   __call = function(self)
     local range = self.get_range()
     if range ~= nil then
-      ts_utils.update_selection(0, range, "v")
+      update_selection(0, range, "v")
     end
   end,
 }
